83e4a2639f165689a6327b7e7b43247f15121826
recep-instructor <125568888+recep-instructor@users.noreply.github.com>
2024-02-15 00:17:44 +0300

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tetris-bucket"
+    bucket         = "tetris-bucket-rcp"
     region         = "us-east-1"
     key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
